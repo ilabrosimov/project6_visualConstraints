c112e9a5dcba4aec59e15438caf25a16f7714729
Ilia <ilabrosimov@gmail.com>
2022-07-07 18:11:59 +0300

    Познакомился с Visual Format Constraints. Anchors. На первый взгляд понятно и просто

--- project6_visualConstraints/ViewController.swift
+++ project6_visualConstraints/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let label1 = UILabel()
+    let label2 = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = .red
+        label1.text = "LABEL1"
+        label1.sizeToFit()
+        
+        
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = .green
+        label2.text = "LABEL2"
+        label2.sizeToFit()
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        
+//        let views = [label1, label2]
+        
+        let viewDictionary = ["label1" : label1, "label2" : label2]
+//
+//        for label in viewDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", metrics: nil, views: viewDictionary))
+//        }
+        var previous : UILabel?
+//
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[label1]-[label2(label1)]-|", metrics: nil, views: viewDictionary))
+//
+        for label in [label1, label2] {
+            
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: view.bounds.height / 2).isActive = true
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 15).isActive = true
+                
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+            }
+            previous = label
+        }
+        label2.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 10).isActive = true
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "ChangeColor", style: .plain, target: self, action: #selector(changeColor))
+        
     }
-
+    @objc func changeColor() {
+        let tempColor = label1.backgroundColor
+        label1.backgroundColor = label2.backgroundColor
+        label2.backgroundColor = tempColor
+        
+    }
 
 }
 
